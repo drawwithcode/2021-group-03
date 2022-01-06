4906a9bad90fe77b69590bae00c13ca1aa4e048a
Lorenzo Bernini <lorenzobernini98@gmail.com>
2022-01-06 11:38:16 +0100

    la mamma di firebase fa il lavoro più antico del mondo

--- firebase-setup.js
+++ firebase-setup.js
@@ -1,11 +1,14 @@
+let allParts;
+let addPart;
+
 // Load and initialize Firebase
 async function firebaseSetup() {
-  // load firebase modules using import("url")
+  // Load firebase modules using import("url")
   const fb_app = "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
   const fb_database =
     "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
 
-  //load libraries
+  //Load libraries
   const { initializeApp } = await import(fb_app);
   const { getDatabase, ref, push, set, onValue } = await import(fb_database);
 
@@ -13,29 +16,41 @@ async function firebaseSetup() {
   const firebaseConfig = {
     apiKey: "AIzaSyCluuzEQ928bZofVPxTafb1RlaM6bK8GT8",
     authDomain: "your-inner-insect.firebaseapp.com",
+    databaseURL:
+      "https://your-inner-insect-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "your-inner-insect",
     storageBucket: "your-inner-insect.appspot.com",
     messagingSenderId: "237974928892",
     appId: "1:237974928892:web:aea547273cb59379bc4679",
-    databaseURL:
-      "https://your-inner-insect-default-rtdb.europe-west1.firebasedatabase.app",
   };
 
   // Initialize App
   const app = initializeApp(firebaseConfig);
 
   // Initialize Database
-  const database = getDatabase(app);
+  const myDatabase = getDatabase(app);
 
-  const prova = ref(database, "risultati");
-  // console.log(prova);
+  //Read test
+  const partRef = ref(myDatabase, "insects/user1/parts/butt");
+  // console.log(partRef);
 
-  let data = {
-    name: "lociobello",
-    score: "43",
-  };
+  onValue(partRef, function (snapshot) {
+    allParts = snapshot.val();
+    console.log(allParts);
+  });
 
-  // prova.push(data);
+  // addPart = function (properties) {
+  //   const newPartRef = push(partRef);
+  //   set(newPartRef, properties);
+  // };
+
+  // const props = {
+  //   text: "test the databse",
+  //   x: 50,
+  //   y: 50,
+  // };
+
+  // addPart(props);
 }
 
 firebaseSetup();
